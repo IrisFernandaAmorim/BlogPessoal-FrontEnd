e8c21e5ab5db8c8dac0a26096008a2297207b35e
IrisFernandaAmorim <amorim.irisf@gmail.com>
2022-09-14 21:16:38 -0300

    consumo API cadastrar

--- src/app/cadastrar/cadastrar.component.ts
+++ src/app/cadastrar/cadastrar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Usuario } from '../model/Usuario';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-cadastrar',
@@ -7,9 +10,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarComponent implements OnInit {
 
-  constructor() { }
+  usuario: Usuario = new Usuario
+  confirmarSenha: string
+  tipUsuario: string
 
-  ngOnInit(): void {
+  constructor(
+    private authService: AuthService, 
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    window.scroll(0,0)
   }
+
+confirmeSenha(event: any) {
+
+  this.confirmarSenha = event.targe.value
+
+}
+
+tipoUsuario(event: any){
+
+  this.tipUsuario = event.targe.value
+
+}
+
+cadastrar(){
+
+this.usuario.tipo = this.tipUsuario
+
+if(this.usuario.senha != this.confirmarSenha){
+  alert('As senhas estão incorretas')
+} else {
+    this.authService.cadastrar(this.usuario).subscribe ((resp: Usuario) =>{
+      this.usuario = resp
+      this.router.navigate(['/entrar'])
+      alert('Usuário cadastrado com sucesso!')
+    })
+
+}
+
+}
 
 }
